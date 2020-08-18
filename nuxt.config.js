5a8ffaec30d25cd0af320fabe2d340be26e917ab
Dmytro Bushchuk <d.bushchuk@outlook.com>
2020-08-18 10:25:19 +0300

    Added working template

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 export default {
-  mode: 'universal',
+  mode: 'spa',
   /*
    ** Headers of the page
    */
@@ -14,7 +14,69 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i&display=swap'
+      },
+      {
+        rel: 'stylesheet',
+        href: './css/open-iconic-bootstrap.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: './css/animate.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: './css/owl.carousel.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: './css/magnific-popup.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: './css/aos.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: './css/ionicons.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: './css/flaticon.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: './css/icomoon.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: './css/style.css'
+      }
+    ],
+    script: [
+      { src: './js/jquery.min.js' },
+      { src: './js/jquery-migrate-3.0.1.min.js' },
+      { src: './js/popper.min.js' },
+      { src: './js/bootstrap.min.js' },
+      { src: './js/jquery.easing.1.3.js' },
+      { src: './js/jquery.waypoints.min.js' },
+      { src: './js/jquery.stellar.min.js' },
+      { src: './js/owl.carousel.min.js' },
+      { src: './js/jquery.magnific-popup.min.js' },
+      { src: './js/aos.js' },
+      { src: './js/jquery.animateNumber.min.js' },
+      { src: './js/scrollax.min.js' },
+      // { src: './js/google-map.js' },
+      { src: './js/main.js' }
+    ]
   },
   /*
    ** Customize the progress-bar color
@@ -31,12 +93,7 @@ export default {
   /*
    ** Nuxt.js dev-modules
    */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module'
-  ],
+  buildModules: [],
   /*
    ** Nuxt.js modules
    */
@@ -56,6 +113,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend (config, ctx) {}
   }
 }
