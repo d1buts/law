6a670976b9b1acd36dbe31c7ae30e8f605811e14
KostiantynKukh <kkuh1993@gmail.com>
2020-11-27 15:49:25 +0200

    added labels

--- components/Consultation.vue
+++ components/Consultation.vue
@@ -8,19 +8,23 @@
           <h2 class="mb-4">Запис на консультацію</h2>
           <form action="#" class="consultation">
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Ваше ім'я">
+              <label class="form-label">Ваше ім'я *</label>
+              <input type="text" class="form-control form-name" placeholder="Введіть ваше ім'я">
             </div>
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Телефон">
+              <label class="form-label">Ваш номер телефону *</label>
+              <input type="text" class="form-control" placeholder="0991234567">
             </div>
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Тема">
+              <label class="form-label">Тема повідомлення</label>
+              <input type="text" class="form-control" placeholder="Супровід бізнесу ...">
             </div>
-            <div class="form-group">
+            <div class="form-group">              
+              <label class="form-label">Повідомлення</label>
               <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Повідомлення" />
             </div>
             <div class="form-group">
-              <input type="submit" value="Send message" class="btn btn-primary py-3 px-4">
+              <input type="submit" value="Відправити" class="btn btn-primary py-3 px-4">
             </div>
           </form>
         </div>
@@ -36,5 +40,14 @@ export default {
 </script>
 
 <style scoped>
+
+.form-label {
+  margin-bottom: 0;
+  font-size: 14px;
+  padding-left: 5px;
+  color: #fff;
+  text-shadow: 2px 1px 5px black;  
+}
+
 
 </style>
